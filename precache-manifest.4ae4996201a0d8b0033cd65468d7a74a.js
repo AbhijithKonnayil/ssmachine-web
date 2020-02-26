94b417389dc5d122ada7084f2b8dc0ed61005615
AbhijithKonnayil <iamabhijith.k@gmail.com>
2020-02-26 13:06:27 +0530

    Updates

---
+++ precache-manifest.4ae4996201a0d8b0033cd65468d7a74a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad7df4744ea84436dd238572371077a",
+    "url": "/ssmachine-web/index.html"
+  },
+  {
+    "revision": "cb49425b1002bccb954b",
+    "url": "/ssmachine-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e922d9d06b22f68a6205",
+    "url": "/ssmachine-web/static/js/2.f3946891.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ssmachine-web/static/js/2.f3946891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb49425b1002bccb954b",
+    "url": "/ssmachine-web/static/js/main.dc7b03eb.chunk.js"
+  },
+  {
+    "revision": "a68d9a2783486d64e6cf",
+    "url": "/ssmachine-web/static/js/runtime-main.c12cf11c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ssmachine-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
