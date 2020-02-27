5e39f86adfaa62ff4927d56c5d2acd28b5a951bc
AbhijithKonnayil <iamabhijith.k@gmail.com>
2020-02-27 17:47:25 +0530

    fixed slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import '../css/sliders.css';
 import SliderImage1 from '../img/slider/slider-img-1.jpg' ;
+import SliderImage2 from '../img/slider/slider-img-2.jpg' ;
+import SliderImage3 from '../img/slider/slider-img-3.jpg' ;
+
+const slideImages = [
+    SliderImage1,SliderImage2,SliderImage3
+  ];
+  
 class Slider extends Component {
     render() {
         return (
-            <div className="single-slider" style={{backgroundImage:`url(${SliderImage1})`}}>
+            <div className="single-slider" style={{  backgroundImage: "url(" + slideImages[this.props.index] + ")" }}>
                 <div className="container">
                     <div className="row">
                         <div className="col-xl-7 offset-xl-1 col-lg-7">
--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import '../css/sliders.css';
 import Slider from './Slider';
-
+import SliderImage1 from '../img/slider/slider-img-1.jpg' 
+import { Slide } from 'react-slideshow-image';
+ 
+const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true,
+    onChange: (oldIndex, newIndex) => {
+      console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+    }
+  }
 
 class Sliders extends Component {
     render() {
         return (
             <div className="slider-area">
-                <div className="slider-active owl-carousel">
-                    <Slider/>
-                    <Slider/>
-                    <Slider/>
-                </div>
+                <Slide {...properties}>
+                    <Slider index="0"/>
+                    <Slider index="1"/>
+                    <Slider index="2"/>
+                </Slide>
             </div>
         );
     }
